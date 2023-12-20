249d5666b851c48e331b94e6bf8c522fd85f4fc8
dmytrawscorpion <dmitriy.murzenkov@scorpion.co>
2023-12-20 16:52:35 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const chunk = new Array(1000000).fill("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {chunk.map((_, index) => (
+        <div key={index}>{index}</div>
+      ))}
+    </>
   );
 }
 
